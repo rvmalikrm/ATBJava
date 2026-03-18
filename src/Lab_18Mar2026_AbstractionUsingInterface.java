f96d07d2a49bba3191ab25cba9ab77e843ddf666
Ravi Malik <rm.rvmalik@gmail.com>
2026-03-18 23:52:32 +0530

    Lab 18Mar 2026 Abstraction using Interface

---
+++ src/Lab_18Mar2026_AbstractionUsingInterface.java
@@ -0,0 +1,15 @@
+public class Lab_18Mar2026_AbstractionUsingInterface {
+    public static void main(String[] args) {
+        Payment payment = new UPI();
+        payment.pay();
+    }
+}
+interface Payment {
+    void pay();
+}
+
+class UPI implements Payment {
+    public void pay() {
+        System.out.println("Paid via UPI");
+    }
+}

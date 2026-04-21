8c2b6e6acfa5010e39eb4cb8a873b8df0f22f841
Ravi Malik <rm.rvmalik@gmail.com>
2026-04-21 12:21:14 +0530

    Lab21_apr_2026_Sort_Array

---
+++ src/April2026/Lab21_apr_2026_Sort_Array.java
@@ -0,0 +1,19 @@
+package April2026;
+public class Lab21_apr_2026_Sort_Array {
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 1, 4};
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted array in ascending order:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}

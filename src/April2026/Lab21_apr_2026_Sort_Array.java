a40ff111cb65d00e143c42a5444d83334f397f18
Ravi Malik <rm.rvmalik@gmail.com>
2026-04-21 12:25:13 +0530

    Initial commit

--- src/April2026/Lab21_apr_2026_Sort_Array.java
+++ src/April2026/Lab21_apr_2026_Sort_Array.java
@@ -1,7 +1,7 @@
 package April2026;
 public class Lab21_apr_2026_Sort_Array {
     public static void main(String[] args) {
-        int[] arr = {5, 2, 8, 1, 4};
+        int[] arr = {5, 2, 8, 1, 4,67,65,34,23,45,76,92,90,19,4,6,8,10,12,11,0,3,9,7,15,14,13,17,16,18,20};
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = i+1; j < arr.length; j++) {
                 if (arr[i] > arr[j]) {
